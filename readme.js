f597a9cf74dfed732715ffe6962a14b1c45a9448
cpu0203 <foto.zolotov@mail.tu>
2023-11-03 23:38:03 +0400

    added readme.js file

---
+++ readme.js
@@ -0,0 +1,15 @@
+
+…or push an existing repository from the command line
+
+git remote add origin https://github.com/cpu0203/MusicJunkBox.git
+git branch -M main
+git push -u origin main
+
+
+
+// WORK REFERENSES
+
+https://github.com/cpu0203/MusicJunkBox
+https://reactnative.dev/docs/pressevent#example
+https://akveo.github.io/react-native-ui-kitten/docs/components/modal/overview#modal
+https://redux-toolkit.js.org/introduction/getting-started#installation
